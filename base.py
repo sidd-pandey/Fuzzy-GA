2f1b046cebe692218db99b7e4718f665986c852b
Pranshu Ranjan Singh <pranshu96ranjan@gmail.com>
2018-09-24 15:34:41 +0800

    Added actual, predicted and predicted actual profit to utils

--- base.py
+++ base.py
@@ -1,7 +1,6 @@
 import pandas as pd
-from utils import load_data, expected_profit_customer
-from tqdm import tqdm
-import numpy as np
+from utils import load_data, expected_profit_campaign_predicted, expected_profit_campaign_actual
+from utils import expected_profit_campaign_predicted_actual, matches_count
 from prediction_system import PredictionModel
 from expert_system import ExpertSystem
 
@@ -10,32 +9,18 @@ from expert_system import ExpertSystem
 df = load_data("data/custdatabase.csv")
 
 model = PredictionModel()
-product_predict = model.predict(df)
-
 expert = ExpertSystem(df)
 
-# Compute expected profit for each customer
-def expected_profit_all_customers(df):    
-    cust_predict = []
-    for index in tqdm(range(len(df))): 
-        row = df.loc[index]
-        cip = expert.predict(row)
-        product = product_predict[index]
-        expected_profit = expected_profit_customer(cip, product)
-        cust_predict.append([df.loc[index, "index"], product, cip, expected_profit])   
-    
-    return cust_predict
- 
-cust_predict = expected_profit_all_customers(df)
-cust_predict_df = pd.DataFrame(cust_predict, columns=["index", "product", "cip", "expected profit"])
-cust_predict_df.to_csv("data/Cust_Predict.csv", index=False)
+expected_profit_campaign_pred, predicted_index = expected_profit_campaign_predicted(model, expert, df, save_csv=True)
+print("Predicted:", expected_profit_campaign_pred)
 
-cust_predict_df_sorted = cust_predict_df.sort_values(by=["expected profit"], ascending=False)
-cust_campaign_400 = cust_predict_df_sorted[:400]
-cust_campaign_400.to_csv("data/Cust_Predict_400.csv", index=False)
+df_actual = pd.read_csv("data/Cust_Actual.csv")
+expected_profit_campaign_act, actual_index = expected_profit_campaign_actual(df_actual)
+print("Actual:", expected_profit_campaign_act)
 
-expected_profit_campaign = np.sum(cust_campaign_400["expected profit"].values)
-print("Predicted:", expected_profit_campaign)
+df_actual = pd.read_csv("data/Cust_Actual.csv", index_col=['index'])
+expected_profit_campaign_pred_act = expected_profit_campaign_predicted_actual(df_actual, predicted_index)
+print("Predicted Actual:", expected_profit_campaign_pred_act)
 
-# cust_actual = pd.read_csv("data/Cust_Actual")
-
+matches = matches_count(actual_index, predicted_index)
+print("Number of Matches:", matches)
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from tqdm import tqdm
+import numpy as np
 
 # Load and preprocess data from file
 def load_data(file):
@@ -24,3 +26,56 @@ def expected_profit_customer(cip, product):
     if product == 'B':
         adj_cip = cip
     return adj_cip
+
+# Compute expected profit for customers selected for campaign
+def expected_profit_campaign_predicted(model, expert, df, save_csv=False):    
+    cust_predict = []
+    product_predict = model.predict(df)
+    for index in tqdm(range(len(df))): 
+        row = df.loc[index]
+        cip = expert.predict(row)
+        product = product_predict[index]
+        expected_profit = expected_profit_customer(cip, product)
+        cust_predict.append([df.loc[index, "index"], product, cip, expected_profit])   
+    
+    cust_predict_df = pd.DataFrame(cust_predict, columns=["index", "product", "cip", "expected profit"])
+    cust_predict_df_sorted = cust_predict_df.sort_values(by=["expected profit"], ascending=False)
+    cust_campaign_400 = cust_predict_df_sorted[:400]
+    
+    if save_csv:
+        cust_predict_df.to_csv("data/Cust_Predict.csv", index=False)
+        cust_campaign_400.to_csv("data/Cust_Predict_400.csv", index=False)
+
+    expected_profit_campaign = np.sum(cust_campaign_400["expected profit"].values)
+
+    return expected_profit_campaign, cust_campaign_400["index"].values
+
+# Compute actual profit for customers selected for campaign
+def expected_profit_campaign_actual(df):
+    profit = []
+    for index in range(len(df)):
+        profit.append(expected_profit_customer(df.loc[index, "cust Investment Potential Score "], 
+            df.loc[index, "status"]))
+    
+    df["profit"] = profit
+    df_sorted = df.sort_values(by=["profit"], ascending=False)
+    df_sorted_400 = df_sorted[:400]
+    expected_profit = np.sum(df_sorted_400["profit"].values)
+    
+    return expected_profit, df_sorted_400["index"].values
+
+# Compute actual profit corresponding to predicted indexes
+def expected_profit_campaign_predicted_actual(df, indexs):
+    profit = []
+    for index in indexs:
+        profit.append(expected_profit_customer(df.loc[index, "cust Investment Potential Score "], 
+            df.loc[index, "status"]))
+    
+    expected_profit = np.sum(profit)
+
+    return expected_profit
+
+# Return the number of matches between actual and predicted
+def matches_count(actual, predicted):
+    matches = set(actual) & set(predicted)
+    return len(matches)
